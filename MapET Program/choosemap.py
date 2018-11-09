383c36fb0e56f3c702fd82aa91e479839668b6f6
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2018-11-09 21:10:38 +0800

    continued choosemap

--- MapET Program/choosemap.py
+++ MapET Program/choosemap.py
@@ -1,5 +1,7 @@
 import os
 import tkinter as tk
+import play
+import mapet
 
 class ChooseMap():
     def __init__(self, parent, file):
@@ -21,7 +23,7 @@ class ChooseMap():
             self.mapbox.append(tk.Frame(self.mapall,width=200,height=200,bd=1,padx=50,pady=50))
             self.mapimg.append(tk.Label(self.mapbox[i],width=20,height=10,bg="white"))
             self.maptext.append(tk.Label(self.mapbox[i],text=mapname,width=10,height=1,font=("arial",20)))
-            self.mapbtn.append(tk.Button(self.mapbox[i],text="Choose map",width=10,height=1,command=openmap,font=("arial",15)))
+            self.mapbtn.append(tk.Button(self.mapbox[i],text="Choose map",width=10,height=1,command=lambda m=mapname:self.openmapf(m),font=("arial",15)))
             self.mapbox[i].grid(column=self.coli,row=self.rowi)  
             self.mapimg[i].grid(column=0,row=1)
             self.maptext[i].grid(column=0,row=2)
@@ -31,18 +33,11 @@ class ChooseMap():
                 self.coli = 0
                 self.rowi += 1
             
-    def openmap():
-
-
-##        self.mapselectbtn = tk.Label(self.buttonframe,text="Map Selection",command=lambda:self.fileopeningf("mapselect"),width=12,padx=10,pady=20,font=("arial",20))
-##        self.mapetbtn = tk.Button(self.buttonframe,text="Map Editor",command=lambda:self.fileopeningf("mapeditor"),width=12,padx=10,pady=20,font=("arial",20))
-##        self.settingsbtn = tk.Button(self.buttonframe,text="Settings",command=lambda:self.fileopeningf("settings"),width=12,padx=10,pady=20,font=("arial",20))
-##        self.programexitbtn = tk.Button(self.buttonframe, text="Quit", command=root.destroy,width=12,padx=10,pady=20, font=("arial",20))
-
-    
-##play.playgamef(root)
-##mapet.Mapedit(root,20,[])
-
-root = tk.Tk()
-ChooseMap(root,"a")
-root.mainloop()
+    def openmapf(self,mapname):
+        print("Opening",mapname)
+        coord = open(os.path.join("maps",mapname,"coord.txt"),"r")
+        if self.file == "mapselect":
+            self.mapall.place_forget()
+            play.playgamef(self.parent)
+        elif self.file == "mapeditor":
+            mapet.Mapedit(self.parent,20,coord)
